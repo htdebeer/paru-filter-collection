633f16d8999a870ef8c5cd259929f7dcd1a7377d
Huub de Beer <Huub@heerdebeer.org>
2017-03-11 15:45:18 +0100

    Added copyright statement and GPL statement to each filter.

--- paru-insert.rb
+++ paru-insert.rb
@@ -1,10 +1,26 @@
 #!/usr/bin/env ruby
+# Copyright 2017 Huub de Beer <Huub@heerdebeer.org>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
 require "paru/filter"
 
 Paru::Filter.run do 
   with "Para" do |paragraph|
     if paragraph.inner_markdown.lines.length == 1
       command, path = paragraph.inner_markdown.strip.split " "
+   
       if command == "::paru::insert"
         markdown = File.read path.gsub(/\\_/, "_")
         paragraph.outer_markdown = markdown
@@ -12,4 +28,3 @@ Paru::Filter.run do
     end
   end
 end
-
